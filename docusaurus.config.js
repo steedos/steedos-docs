f10599b67c39fd049dd7c650ead65a360380d0ed
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-05-25 13:40:30 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Steedos',
+  title: 'Steedos Help',
   tagline: 'Open source alterative to Salesforce Platform.',
   favicon: 'img/favicon.png',
 
