0b157db2fb8c54f3873e9d84cd395d85b36c7c57
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-06-13 11:28:26 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/steedos/steedos-docs/tree/main/',
+            'https://github.com/steedos/steedos-docs/tree/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
