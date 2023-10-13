12a2ca800890f3c6cb79de1b1c10c6622702221c
庄建国 <9340100@qq.com>
2023-10-13 03:42:28 +0000

    根据浏览器语言自动跳转

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import Layout from '@theme/Layout';
 import {Redirect, useLocation} from '@docusaurus/router';
 
 export default function Home() {
-    const userLang = navigator.language || navigator.userLanguage || "zh-CN"; 
+    // const userLang = navigator?.language || navigator?.userLanguage || "zh-CN"; 
+    const userLang = 'zh-CN';
 
     if (userLang.startsWith('zh'))
         return  <Redirect to="/zh-CN/overview" />
