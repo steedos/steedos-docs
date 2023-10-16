b2923496b46603b82411423ccaff4fb157d81db0
庄建国 <9340100@qq.com>
2023-10-16 01:53:08 +0000

    首页根据浏览器语言自动跳转

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import Layout from '@theme/Layout';
-import {Redirect, useLocation} from '@docusaurus/router';
+import React, { useEffect } from 'react';
 
-export default function Home() {
-    // const userLang = navigator?.language || navigator?.userLanguage || "zh-CN"; 
-    const userLang = 'zh-CN';
+function Home() {
+  useEffect(() => {
+    // 这部分代码将在页面加载完成后执行
+    const language = navigator.language || navigator.userLanguage;
 
-    if (userLang.startsWith('zh'))
-        return  <Redirect to="/zh-CN/overview" />
-    else
-        return  <Redirect to="/overview" />
+    // 检查语言是否是中文
+    if (language.includes('zh-CN') || language.includes('zh')) {
+      window.location.href = '/zh-CN/overview';
+    } else {
+      // 为其他语言添加更多条件或保留为默认语言
+      window.location.href = '/overview';  // 示例
+    }
+  }, []);
+
+  // 页面内容（如果有的话）在重定向发生之前可能会短暂显示
+  return <div></div>;
 }
 
+export default Home;
