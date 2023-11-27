416db17234e6bf175c050dad6926a601ffe5f4b6
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-11-27 18:25:58 +0800

    区分中英文站点

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,11 @@ import React, { useEffect } from 'react';
 function Home() {
   useEffect(() => {
     // 这部分代码将在页面加载完成后执行
-    const language = navigator.language || navigator.userLanguage;
+    // const language = navigator.language || navigator.userLanguage;
+    const language = window.location.host?.includes('.cn') ? 'zh-CN' : 'en';
 
     // 检查语言是否是中文
-    if (language.includes('zh-CN') || language.includes('zh')) {
+    if (language.includes('zh-CN')) {
       window.location.href = '/zh-CN/getting-started';
     } else {
       // 为其他语言添加更多条件或保留为默认语言
