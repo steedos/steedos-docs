88a9efa207a492e1a8dbeb21a3c0445e775ca9cc
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2024-04-25 12:00:18 +0800

    优化首页

---
+++ src/theme/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Footer from '@theme-original/Footer';
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <div class="absolute z-40 lg:z-50 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none">
+        <div class="w-[108rem] flex-none flex justify-end">
+          <picture>
+            <source srcset="/img/beams/docs@30.avif" type="image/avif"/>
+            <img src="/img/beams/docs@tinypng.png" alt="" class="w-[71.75rem] flex-none max-w-none dark:hidden"/>
+          </picture>
+          <picture>
+            <source srcset="/img/beams/docs-dark@30.avif" type="image/avif"/>
+            <img src="/img/beams/docs-dark@tinypng.png" alt="" class="w-[90rem] flex-none max-w-none hidden dark:block"/>
+          </picture>
+        </div>
+      </div>
+      <Footer {...props} />
+    </>
+  );
+}
